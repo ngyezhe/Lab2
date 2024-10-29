915976b1bea150a0998755891208bd73d53d4274
Ye Zhe <NGYZ.23@ichat.sp.edu.sg>
2024-10-29 10:37:52 +0800

    bmi

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,35 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def funcName(parameter1, parameter2) :
+    print ("this is a dummy function")
+    return 10
+
+def display_main_menu():
+    print ("Enter some numbers separated by commas (e.g 5, 67, 32)")
+
+    x = input()
+    num = x.split(',')
+    print(num)
+
+def calc_average():
+    print("calc_average")
+
+def get_user_input():
+    print("get_user_input")
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+
+def calc_average_temperature(num):
+    print("calc_average_tempature")
+    return sum(num) / len(num)
+
+def calc_min_max_temperature(num):
+    print("calc_min_max_temperature")
+    return [min(num), max[num]]
--- bmi.py
+++ bmi.py
@@ -6,4 +6,16 @@ def calculator_bmi(height, weight):
 
     print ("BMI =" +str(bmi))
 
+    if bmi < 18.5:
+        print("Under weight")
+        return -1
+    elif bmi > 18.5:
+        print("Normal weight")
+        return 0
+    else:
+        print("Over weight")
+        return 1
+
+
 calculator_bmi(weight=57, height=1.73)
+
